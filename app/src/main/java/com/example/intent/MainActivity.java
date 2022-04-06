b9b2ef07040318777c77d7c826192e020ee2c5c8
Никита <pedichn@gmail.com>
2022-04-06 11:17:04 +0300

    Почти всё

--- app/src/main/java/com/example/intent/MainActivity.java
+++ app/src/main/java/com/example/intent/MainActivity.java
@@ -2,7 +2,16 @@ package com.example.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.time.Duration;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+            setContentView(R.layout.activity_main);
+            Intent intent = getIntent();
+            String getMsg = intent.getStringExtra("message");
+            ((EditText) findViewById(R.id.editTextFirst)).setText(getMsg);
+            if (getMsg != null)
+            Toast.makeText(getApplicationContext(), getMsg, Toast.LENGTH_SHORT).show();
+
     }
+    public void onSendMessage(View view) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        String getMessage = ((EditText)findViewById(R.id.editTextFirst)).getText().toString();
+        intent.putExtra("message", getMessage);
+        startActivity(intent);
+        finish();
+    }
+    public void exit(View view)
+    {
+        AlertDialog.Builder a_builder = new AlertDialog.Builder(MainActivity.this);
+
+        a_builder.setMessage(getString(R.string.ExitMessage)).setIcon(R.drawable.alert).setNegativeButton("Отмена", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {dialog.cancel();}}).setPositiveButton("Выйти", new DialogInterface.OnClickListener() {
+        public void onClick(DialogInterface dialog, int id) {finish();}});
+        a_builder.show();
+
+    }
+
+
 }
--- app/src/main/java/com/example/intent/MainActivity2.java
+++ app/src/main/java/com/example/intent/MainActivity2.java
@@ -2,7 +2,11 @@ package com.example.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Intent intent = getIntent();
+        String getMsg = intent.getStringExtra("message");
+        ((EditText)findViewById(R.id.editTextSecond)).setText(getMsg);
     }
+    public void onSendMessage(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        String getMessage = ((EditText)findViewById(R.id.editTextSecond)).getText().toString();
+        intent.putExtra("message", getMessage);
+        startActivity(intent);
+        finish();
+    }
+    public void cancel(View view) {
+        Intent oldintent = getIntent();
+        String getMsg = oldintent.getStringExtra("message");
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("message", getMsg);
+        startActivity(intent);
+        finish();
+    }
+
 }
